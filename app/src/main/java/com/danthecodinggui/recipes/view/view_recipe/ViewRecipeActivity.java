6c3b3d16f3cf7c61d34c677c6947bff43b292b6a
Dan <psydb3@nottingham.ac.uk>
2018-02-20 23:22:07 +0000

    Complete view recipe activity collapsing toolbar with embedded tab layout

--- app/src/main/java/com/danthecodinggui/recipes/view/view_recipe/ViewRecipeActivity.java
+++ app/src/main/java/com/danthecodinggui/recipes/view/view_recipe/ViewRecipeActivity.java
@@ -2,7 +2,8 @@ package com.danthecodinggui.recipes.view.view_recipe;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
+
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -10,15 +11,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
-import android.view.Window;
-import android.view.WindowManager;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.danthecodinggui.recipes.R;
 
-public class ViewRecipeActivity extends AppCompatActivity {
+public class ViewRecipeActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
 
-    private int mutedColor;
+    private RelativeLayout previewContainer;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +29,28 @@ public class ViewRecipeActivity extends AppCompatActivity {
         //TODO need to conditionally set this between recipe with/without photo, ie normal toolbar for no photo
         setContentView(R.layout.activity_view_recipe);
 
-        Toolbar toolbar = findViewById(R.id.tbar_vw_recipe);
+        toolbar = findViewById(R.id.tbar_vw_recipe);
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null)
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        final CollapsingToolbarLayout collapsingToolbar = findViewById(R.id.ctl_vw_recipe);
+        CollapsingToolbarLayout collapsingToolbar = findViewById(R.id.ctl_vw_recipe);
         collapsingToolbar.setTitle(getIntent().getStringExtra("Title"));
 
-        ImageView header = findViewById(R.id.ivw_toolbar_preview);
+        final AppBarLayout appBarLayout = findViewById(R.id.abl_view_recipe);
+        appBarLayout.addOnOffsetChangedListener(this);
 
+        previewContainer = findViewById(R.id.rly_preview_container);
+        tabLayout = findViewById(R.id.tly_view_recipe);
+
+        //Generates one colour from the recipe preview photo to use as toolbar colour
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
                 R.drawable.sample_image);
         Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
             @Override
             public void onGenerated(Palette palette) {
-                mutedColor = palette.getMutedColor(R.attr.colorPrimary);
-                collapsingToolbar.setContentScrimColor(mutedColor);
+                int mutedColor = palette.getMutedColor(R.attr.colorPrimary);
+                appBarLayout.setBackgroundColor(mutedColor);
             }
         });
 
@@ -50,12 +58,17 @@ public class ViewRecipeActivity extends AppCompatActivity {
         tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.tab_ingredients)));
         tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.tab_method)));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        /*
+        For adding icons to tabs later
+        TODO Once you're at this stage, look into animated icons like in telegramX
+        tabLayout.getTabAt(0).setIcon();
+        tabLayout.getTabAt(1).setIcon();
+        */
 
         final ViewPager viewPager = findViewById(R.id.vpr_view_recipe);
         final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
@@ -63,16 +76,24 @@ public class ViewRecipeActivity extends AppCompatActivity {
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {}
-
             @Override
             public void onTabReselected(TabLayout.Tab tab) {}
         });
-
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         finish();
         return true;
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        int toolBarHeight = toolbar.getMeasuredHeight();
+        int appBarHeight = appBarLayout.getMeasuredHeight();
+        float transitionSpace = (float) appBarHeight - toolBarHeight - tabLayout.getMeasuredHeight();
+        Float f = ((transitionSpace + verticalOffset) / transitionSpace) * 255;
+        Log.d("graphics", "f: " + f.toString());
+        previewContainer.getBackground().setAlpha(Math.round(f));
     }
 }
