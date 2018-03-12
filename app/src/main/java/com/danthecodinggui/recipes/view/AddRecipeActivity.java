b1d849b720c78a36425e6a01badebfaf68a532cc
Dan <psydb3@nottingham.ac.uk>
2018-03-12 20:13:58 +0000

    Can add ingredient to a list

--- app/src/main/java/com/danthecodinggui/recipes/view/AddRecipeActivity.java
+++ app/src/main/java/com/danthecodinggui/recipes/view/AddRecipeActivity.java
@@ -4,20 +4,48 @@ import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.danthecodinggui.recipes.R;
 import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
 import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
 import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddRecipeActivity extends AppCompatActivity {
+
+    private RecyclerView ingredientsView;
+    private IngredientsViewAdapter ingredientsAdapter;
+    private List<String> ingredientsList;
+    private EditText addIngredient;
+
+    private RecyclerView methodView;
+    private List<String> methodSteps;
+    private EditText addStep;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+
+        ingredientsView = findViewById(R.id.rvw_add_ingredients);
+        ingredientsAdapter = new IngredientsViewAdapter();
+        ingredientsList = new ArrayList<>();
+        ingredientsView.setAdapter(ingredientsAdapter);
+        ingredientsView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        addIngredient = findViewById(R.id.etxt_add_ingredient);
+        addIngredient.setOnKeyListener(newIngredientListener);
 
         BuildFabMenu();
     }
@@ -87,5 +115,55 @@ public class AddRecipeActivity extends AppCompatActivity {
                 animation.start();
             }
         });
+    }
+
+    private void addIngredient() {
+        String ingredient = addIngredient.getText().toString();
+        ingredientsList.add(ingredient);
+        ingredientsAdapter.notifyItemChanged(ingredientsList.size());
+
+        addIngredient.getText().clear();
+    }
+
+    TextView.OnKeyListener newIngredientListener = new TextView.OnKeyListener() {
+        @Override
+        public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+            //Detect when enter pressed while in edittext
+            if (keyEvent.getAction() == KeyEvent.ACTION_DOWN
+                && keyCode == KeyEvent.KEYCODE_ENTER) {
+                addIngredient();
+                return true;
+            }
+            return false;
+        }
+    };
+
+    class IngredientsViewAdapter extends RecyclerView.Adapter<IngredientsViewAdapter.IngredientViewHolder> {
+
+        @Override
+        public IngredientViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new IngredientViewHolder(LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.ingredient_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(IngredientViewHolder holder, int position) {
+            holder.ingredient.setText(getString(R.string.txt_ingredient_item, ingredientsList.get(position)));
+        }
+
+        @Override
+        public int getItemCount() {
+            return ingredientsList.size();
+        }
+
+        class IngredientViewHolder extends RecyclerView.ViewHolder{
+
+            TextView ingredient;
+
+            IngredientViewHolder(View itemView) {
+                super(itemView);
+                ingredient = itemView.findViewById(R.id.txt_method_item);
+            }
+        }
     }
 }
